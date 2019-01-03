9d9c513d67ba26ec15cc2010f77ca12d303c5310
David Ingraham <david@spektreworks.com>
2019-01-03 10:10:26 -0700

    First Complete Implementation

---
+++ cot_schema/README.md
@@ -0,0 +1,1 @@
+I can not seem to find the the official COT schema on the web as of 2019. Here are a few parts of it that I have found floting around.
--- mavcot.py
+++ mavcot.py
@@ -1,9 +1,14 @@
 from pymavlink import mavutil
-import pycot, os, datetime
+import pycot, os, time, datetime, socket, uuid, geoid
 
-#Assert Mavlink 2
+# Configure Socket Connection
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+address = ('192.168.2.47', 9190)
+
+# Assert Mavlink 2
 os.environ['mavlink20'] = "1"
 
+print("Waiting for MAVLink Socket")
 mav = mavutil.mavlink_connection("udp:127.0.0.1:14550", retries=20)
 
 print("Connecting, waiting for heartbeat")
@@ -13,19 +18,44 @@ mav.wait_gps_fix()
 print("Fix acquired, running")
 
 point = pycot.Point()
-point.ce = 0.1
-point.le = 0.1
+point.ce = 1.0
+point.le = 1.0
 event = pycot.Event()
-event.version='0.1'
-event.event_type = 'a-h-G-p-i'
-event.uid = uuid.uuid4()
-event.how="h-e"
+event.version='2.0'
+event.event_type = 'a-f-A-M-F-Q'
+
+event.uid = "Cobalt004.1234" 
+event.how="m-g"
+
+geoid = geoid.GeoidHeight()
+
+def get_geoid_height(lat, lon):
+	geoid_height = 0
+	try:
+		geoid_height = geoid.get(lat, lon)
+	except Exception as e:
+		print(e)
+	return geoid_height
+
+last_sent_time = time.time()
 
 while True:
-	msg = mav.recv_match(type='GPS_RAW_INT')
-	point.lat = msg.lat / 10000.0
-	point.lon = msg.lon / 10000.0
-	point.hae = msg.alt / 1000.0 - 8 
-	event.point = point
-	event.time = datetime.datetime.fromtimestamp(msg.time_usec / 1000.0)
-	print(event.render(standalone=True, pretty=True))
+	msg = mav.recv_match(type='GLOBAL_POSITION_INT')
+	if msg is not None and ((time.time() - last_sent_time) > 1):
+		#print(msg)
+		point.lat = msg.lat / 10000000.0
+		point.lon = msg.lon / 10000000.0
+		alt_msl = msg.alt / 1000.0 
+		point.hae = alt_msl + get_geoid_height(point.lat,point.lon)
+		event.point = point
+		event.time = datetime.datetime.fromtimestamp(time.time())
+		event_xml = event.render(standalone=True, pretty=True)
+		print(event_xml)
+		try:
+			s.sendto(event_xml, address)
+			last_sent_time =time.time()
+		except Exception as e:
+			print(e)
+			time.sleep(1) # Never run faster than 1 Hz when the TCP connection is closed
+
+
